fe9ed3babc7c323cec13f0c9c2d8785bf4a48756
CORN-玉米王 <kuzoncby@gmail.com>
2023-02-17 02:25:51 +0700

    💾

---
+++ Server/Hubs/ChatHub.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorWebAssemblySignalRApp.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly ILogger _logger;
+
+        public ChatHub(ILogger<ChatHub> logger)
+        {
+            _logger = logger;
+        }
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task FileUpload(string fileName, int percent, byte[] file)
+        {
+            _logger.LogInformation("recieved_file " + fileName + ", percent " + percent);
+
+            var filePath = @"C:\\Users\\kuzon\\Documents\\" + fileName;
+
+            if (percent == 0 && System.IO.File.Exists(filePath))
+            {
+                System.IO.File.Delete(filePath);
+            }
+
+            using (var fileStream = new FileStream(filePath, FileMode.Append, FileAccess.Write))
+            {
+                using (var bw = new BinaryWriter(fileStream))
+                {
+                    await fileStream.WriteAsync(file, 0, file.Length);
+                    await Clients.All.SendAsync("ReceiveMessage", "System", "Recieved percent: " + percent);
+                }
+            }
+        }
+    }
+}
