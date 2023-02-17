78860b2030e252e09eb6940f902610041089c9a6
CORN-玉米王 <kuzoncby@gmail.com>
2023-02-17 15:28:20 +0700

    💾

--- Server/Hubs/ChatHub.cs
+++ Server/Hubs/ChatHub.cs
@@ -15,27 +15,61 @@ namespace BlazorWebAssemblySignalRApp.Server.Hubs
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
 
-        public async Task FileUpload(string fileName, int percent, byte[] file)
+        public async Task FileUpload(string fileName, int index, byte[] fragment)
+        {
+            string[] folderPaths = { Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), fileName };
+
+            var folderPath = System.IO.Path.Combine(folderPaths);
+
+            if (!System.IO.Directory.Exists(folderPath))
+            {
+                System.IO.Directory.CreateDirectory(folderPath);
+            }
+
+            string[] paths = { folderPath, index.ToString() };
+
+            var filePath = System.IO.Path.Combine(paths);
+
+            _logger.LogInformation("recieved_file " + filePath);
+
+            if (System.IO.File.Exists(filePath))
+            {
+                System.IO.File.Delete(filePath);
+            }
+
+            using (var fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+            {
+                using (var bw = new BinaryWriter(fileStream))
+                {
+                    await fileStream.WriteAsync(fragment, 0, fragment.Length);
+                    await Clients.All.SendAsync("ReceiveMessage", "File: ", "Recieved fragment: " + index);
+                }
+            }
+        }
+
+        public async Task FileMerge(string fileName)
         {
             string[] paths = { Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), fileName };
 
             var filePath = System.IO.Path.Combine(paths);
 
-            _logger.LogInformation("recieved_file " + filePath + ", percent " + percent);
+            _logger.LogInformation("merge_file " + filePath);
 
-            if (percent == 0 && System.IO.File.Exists(filePath))
+            string[] inputFilePaths = Directory.GetFiles(filePath);
+            _logger.LogInformation("Files in directory, {0}", inputFilePaths);
+            using (var outputStream = File.Create(filePath))
             {
-                System.IO.File.Delete(filePath);
-            }
-
-            using (var fileStream = new FileStream(filePath, FileMode.Append, FileAccess.Write))
-            {
-                using (var bw = new BinaryWriter(fileStream))
+                foreach (var inputFilePath in inputFilePaths)
                 {
-                    await fileStream.WriteAsync(file, 0, file.Length);
-                    await Clients.All.SendAsync("ReceiveMessage", "System", "Recieved percent: " + percent);
+                    using (var inputStream = File.OpenRead(inputFilePath))
+                    {
+                        inputStream.CopyTo(outputStream);
+                    }
+                    _logger.LogInformation("The file {0} has been processed.", inputFilePath);
                 }
             }
+
+            await Clients.All.SendAsync("ReceiveMessage", "File merged", fileName);
         }
     }
 }
